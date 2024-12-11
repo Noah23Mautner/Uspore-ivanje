39e5e0d14c8571d4ce2f400e719df2c5c23e160e
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-12-11 09:18:44 +0100

    Add files via upload

---
+++ Uspoređivanje/Uspoređivanje/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uspoređivanje
+{
+    internal class Book
+    {
+        
+        public int BookID { get; set; }
+        public String Name { get; set; }
+        public int Image { get; set; }
+        public Book(int bookId, string name, int image)
+        {
+            this.BookID = bookId;
+            this.Name = name;
+            this.Image = image;
+        }
+    }
+}
---
+++ Uspoređivanje/Uspoređivanje/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Uspoređivanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XDocument booksFromFile = XDocument.Load(@"prvi.xml");
+            Console.WriteLine("Sadržaj od prvi.xml:");
+            Console.WriteLine(booksFromFile);
+
+            XDocument booksFromFile1 = XDocument.Load(@"drugi.xml");
+            Console.WriteLine("Sadržaj od drugi.xml:");
+            Console.WriteLine(booksFromFile1);
+
+            var prviBooks = booksFromFile.Descendants("book")
+               .Select(b => new
+               {
+                   Id = (string)b.Attribute("id"),
+                   Image = (string)b.Attribute("image"),
+                   Name = (string)b.Attribute("name")
+               }).ToList();
+
+            var drugiBooks = booksFromFile1.Descendants("book")
+                .Select(b => new
+                {
+                    Id = (string)b.Attribute("id"),
+                    Image = (string)b.Attribute("image"),
+                    Name = (string)b.Attribute("name")
+                }).ToList();
+
+            var differentBooks = prviBooks
+                .Union(drugiBooks)
+                .GroupBy(b => b.Id)
+                .Where(g => g.Count() == 1 || g.First().Image != g.Last().Image || g.First().Name != g.Last().Name)
+                .SelectMany(g => g)
+                .ToList();
+
+            if (differentBooks.Any())
+            {
+                Console.WriteLine("Razlike između XML dokumenata:");
+                foreach (var book in differentBooks)
+                {
+                    Console.WriteLine($"ID: {book.Id}, Image: {book.Image}, Name: {book.Name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nema razlika između XML dokumenata.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
